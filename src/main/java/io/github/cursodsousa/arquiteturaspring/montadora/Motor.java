16c2f5735e626240391e4bfa3d2256a43dd8e37a
Andre Schons <andreferens86@gmail.com>
2026-01-05 09:32:14 -0300

    Add enums e entidades

---
+++ src/main/java/io/github/cursodsousa/arquiteturaspring/montadora/Motor.java
@@ -0,0 +1,49 @@
+package io.github.cursodsousa.arquiteturaspring.montadora;
+
+public class Motor {
+    private String modelo;
+    private Integer cavalos;
+    private Integer cilindros;
+    private Double litragem;
+    private TipoDeMotor tipo;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public Integer getCavalos() {
+        return cavalos;
+    }
+
+    public void setCavalos(Integer cavalos) {
+        this.cavalos = cavalos;
+    }
+
+    public Integer getCilindros() {
+        return cilindros;
+    }
+
+    public void setCilindros(Integer cilindros) {
+        this.cilindros = cilindros;
+    }
+
+    public Double getLitragem() {
+        return litragem;
+    }
+
+    public void setLitragem(Double litragem) {
+        this.litragem = litragem;
+    }
+
+    public TipoDeMotor getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoDeMotor tipo) {
+        this.tipo = tipo;
+    }
+}
---
+++ src/main/java/io/github/cursodsousa/arquiteturaspring/montadora/TipoDeMotor.java
@@ -0,0 +1,7 @@
+package io.github.cursodsousa.arquiteturaspring.montadora;
+
+public enum TipoDeMotor {
+    ASPIRADO,
+    TURBO,
+    ELETRICO
+}
